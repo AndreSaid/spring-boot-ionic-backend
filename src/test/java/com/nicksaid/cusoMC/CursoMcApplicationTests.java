a4d9a1e0f2469983db7a398de7224baec550708c
Andre Said <andresaid123@hotmail.com>
2020-07-28 09:18:31 -0300

    Projeto criado

---
+++ src/test/java/com/nicksaid/cusoMC/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicksaid.cusoMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
