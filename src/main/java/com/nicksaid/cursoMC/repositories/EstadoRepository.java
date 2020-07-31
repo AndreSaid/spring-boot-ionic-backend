d7d73a185ea194910f51d75725781a37e68d8aee
Andre Said <andresaid123@hotmail.com>
2020-07-31 09:44:19 -0300

    Estado e cidade

---
+++ src/main/java/com/nicksaid/cursoMC/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.nicksaid.cursoMC.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nicksaid.cursoMC.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
