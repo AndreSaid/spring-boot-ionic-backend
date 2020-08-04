f08ba221bebd3bc131312b7d60cd584cab428c80
Andre Said <andresaid123@hotmail.com>
2020-08-04 16:41:34 -0300

    Inserindo Pedido

--- src/main/java/com/nicksaid/cursoMC/resources/PedidoResource.java
+++ src/main/java/com/nicksaid/cursoMC/resources/PedidoResource.java
@@ -1,13 +1,21 @@
 package com.nicksaid.cursoMC.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nicksaid.cursoMC.domain.Categoria;
 import com.nicksaid.cursoMC.domain.Pedido;
+import com.nicksaid.cursoMC.dto.CategoriaDTO;
 import com.nicksaid.cursoMC.services.PedidoService;
 
 @RestController
@@ -21,7 +29,15 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> find(@PathVariable Integer id){
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
 	}
+	
+
+	@RequestMapping(method= RequestMethod.POST)//metodo POST dentro das boas praticas
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		}
 
 }
