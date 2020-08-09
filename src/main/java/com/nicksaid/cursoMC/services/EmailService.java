a59c868539f47b7cacdff5831eaa4e7782c536ce
Andre Said <andresaid123@hotmail.com>
2020-08-09 09:50:33 -0300

    Email HTML

--- src/main/java/com/nicksaid/cursoMC/services/EmailService.java
+++ src/main/java/com/nicksaid/cursoMC/services/EmailService.java
@@ -1,12 +1,18 @@
 package com.nicksaid.cursoMC.services;
 
-import com.nicksaid.cursoMC.domain.Pedido;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
+
+import com.nicksaid.cursoMC.domain.Pedido;
 
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
