b27ef50bb4d65c03a484c0d97fdb273ef0cbbf1d
Andre Said <andresaid123@hotmail.com>
2020-08-12 12:02:54 -0300

    Refresh token

---
+++ src/main/java/com/nicksaid/cursoMC/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.nicksaid.cursoMC.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nicksaid.cursoMC.security.JWTUtil;
+import com.nicksaid.cursoMC.security.UserSS;
+import com.nicksaid.cursoMC.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+	UserSS user = UserService.authenticated();
+	String token = jwtUtil.generateToken(user.getUsername());
+	response.addHeader("Authorization", "Bearer " + token);
+	return ResponseEntity.noContent().build();
+	}
+}
