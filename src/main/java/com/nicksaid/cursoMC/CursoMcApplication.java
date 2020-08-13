c6f2c1f3a118c9fc793a654d969a67f4ff8e2021
Andre Said <andresaid123@hotmail.com>
2020-08-13 09:48:47 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/nicksaid/cursoMC/CursoMcApplication.java
+++ src/main/java/com/nicksaid/cursoMC/CursoMcApplication.java
@@ -1,11 +1,17 @@
 package com.nicksaid.cursoMC;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nicksaid.cursoMC.services.S3Service;
+
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -14,7 +20,7 @@ public class CursoMcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\temp\\fotos\\joao.jpg");
 		
 		
 	}
