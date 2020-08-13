370adebe412b03c5698eddd4bc0fe3646c4b9f86
Andre Said <andresaid123@hotmail.com>
2020-08-13 14:20:37 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/nicksaid/cursoMC/domain/Cliente.java
+++ src/main/java/com/nicksaid/cursoMC/domain/Cliente.java
@@ -54,8 +54,6 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-
-	private String imageUrl;
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -179,12 +177,5 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
 
 }
